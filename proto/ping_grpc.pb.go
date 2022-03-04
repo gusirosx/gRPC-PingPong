b4eec6b3bb7afcf923e6836abc311396aa13b89a
Gustavo Rodrigues <gsrodrigues280@gmail.com>
2022-03-03 21:17:02 -0300

    removing send upstream

--- proto/ping_grpc.pb.go
+++ proto/ping_grpc.pb.go
@@ -19,7 +19,6 @@ const _ = grpc.SupportPackageIsVersion7
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type PingServiceClient interface {
 	Send(ctx context.Context, in *Request, opts ...grpc.CallOption) (*Response, error)
-	SendUpstream(ctx context.Context, in *Request, opts ...grpc.CallOption) (*Response, error)
 }
 
 type pingServiceClient struct {
@@ -39,21 +38,11 @@ func (c *pingServiceClient) Send(ctx context.Context, in *Request, opts ...grpc.
 	return out, nil
 }
 
-func (c *pingServiceClient) SendUpstream(ctx context.Context, in *Request, opts ...grpc.CallOption) (*Response, error) {
-	out := new(Response)
-	err := c.cc.Invoke(ctx, "/proto.PingService/SendUpstream", in, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
 // PingServiceServer is the server API for PingService service.
 // All implementations must embed UnimplementedPingServiceServer
 // for forward compatibility
 type PingServiceServer interface {
 	Send(context.Context, *Request) (*Response, error)
-	SendUpstream(context.Context, *Request) (*Response, error)
 	mustEmbedUnimplementedPingServiceServer()
 }
 
@@ -63,9 +52,6 @@ type UnimplementedPingServiceServer struct {
 
 func (UnimplementedPingServiceServer) Send(context.Context, *Request) (*Response, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Send not implemented")
-}
-func (UnimplementedPingServiceServer) SendUpstream(context.Context, *Request) (*Response, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method SendUpstream not implemented")
 }
 func (UnimplementedPingServiceServer) mustEmbedUnimplementedPingServiceServer() {}
 
@@ -98,24 +84,6 @@ func _PingService_Send_Handler(srv interface{}, ctx context.Context, dec func(in
 	return interceptor(ctx, in, info, handler)
 }
 
-func _PingService_SendUpstream_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(Request)
-	if err := dec(in); err != nil {
-		return nil, err
-	}
-	if interceptor == nil {
-		return srv.(PingServiceServer).SendUpstream(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: "/proto.PingService/SendUpstream",
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(PingServiceServer).SendUpstream(ctx, req.(*Request))
-	}
-	return interceptor(ctx, in, info, handler)
-}
-
 // PingService_ServiceDesc is the grpc.ServiceDesc for PingService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -126,10 +94,6 @@ var PingService_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "Send",
 			Handler:    _PingService_Send_Handler,
-		},
-		{
-			MethodName: "SendUpstream",
-			Handler:    _PingService_SendUpstream_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
